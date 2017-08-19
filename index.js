d3ef75aadf8613add93a869da916a041c432000f
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-08-19 13:35:43 -0500

    trigger y convierte a mayuscula

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ exports.addMessage = functions.https.onRequest((req, res) => {
         res.redirect(303, snapshot.ref);
       });
     });
+
+exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+    .onWrite(event => {
+      const original = event.data.val();
+      console.log('Uppercasing', event.params.pushId, original);
+      const uppercase = original.toUpperCase();
+       return event.data.ref.parent.child('uppercase').set(uppercase);
+    });
