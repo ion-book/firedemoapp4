ce32e887318913c518c412163255fde01fe31c63
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-08-19 13:33:36 -0500

    addMessage

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
+
+// Toma el texto pasado en el endpoind httpsy los escribe en el Realtime. /messages/:pushId/original
+
+exports.addMessage = functions.https.onRequest((req, res) => {
+
+   
+      const original = req.query.text;
+   
+      admin.database().ref('/messages').push({original: original}).then(snapshot => {
+        res.redirect(303, snapshot.ref);
+      });
+    });
